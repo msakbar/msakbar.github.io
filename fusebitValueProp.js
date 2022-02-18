d834ce94a5d4079c4e935e468d8deacab7fbd3cd
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-02-19 03:21:31 +0400

    fusebitValueProp

---
+++ fusebitValueProp.js
@@ -0,0 +1,5 @@
+export default function fusebitValueProp () {
+  console.log('Fusebit is a Powerful Integration Platform for Developers');
+  console.log('Our code-first integration platform and SaaS connectors remove the headaches of dealing with many different APIs.');
+  console.log('Get started with your first integration at fusebit.io');
+}
